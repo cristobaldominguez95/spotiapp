cf5b1e045aa7fdb8ab366fea9cf9ebfa93082658
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-02-03 22:50:11 +0100

    Create requests to get artist info and top tracks

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,19 +6,35 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class SpotifyService {
 
-  private baseUrl: string = 'https://api.spotify.com';
-  private token: string = 'BQB286kvt_rd743M0n6N2UCGOplgZ0s1KvGvopOuPl-bTPkhNGNMyTshokozlR3esPpjn7AUpIH1_qP4t48';
+  private baseUrl: string = 'https://api.spotify.com/v1';
+  private token: string = 'BQB_tF-0-Q5U5cT8jwznWkLRUlZgHDLe3QtbHkUPf0DPNkVcow66kKaRuvw2XhtG9QzpwULvdxSIB5AVtLo';
 
   constructor(private httpClient: HttpClient) { }
 
   getArtists(artist: string) {
-    let url = `${this.baseUrl}/v1/search?query=${artist}&type=artist&limit=20`;
+    let url = `${this.baseUrl}/search?query=${artist}&type=artist&limit=20`;
     let headers = new HttpHeaders({
       Authorization: `Bearer ${this.token}`
     });
     return this.httpClient.get(url, { headers }).map((data: any) => {
       return data.artists.items;
     });
+  }
+
+  getArtist(id: string) {
+    let url = `${this.baseUrl}/artists/${id}`;
+    let headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`
+    });
+    return this.httpClient.get(url, { headers });
+  }
+
+  getTopTracks(id: string) {
+    let url = `${this.baseUrl}/artists/${id}/top-tracks?country=ES`;
+    let headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`
+    });
+    return this.httpClient.get(url, { headers });
   }
 
 }
